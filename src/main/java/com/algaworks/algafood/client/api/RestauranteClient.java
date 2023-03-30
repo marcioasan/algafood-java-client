edc7145eb85136fbff9b7cb11bcd702ffb980905
Marcio Antonio dos Santos <marcioasan@gmail.com>
2023-03-30 10:33:20 -0300

    16.12. Desafio: Implementando uma requisição POST no client Java

--- src/main/java/com/algaworks/algafood/client/api/RestauranteClient.java
+++ src/main/java/com/algaworks/algafood/client/api/RestauranteClient.java
@@ -4,9 +4,13 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
+import com.algaworks.algafood.client.model.RestauranteModel;
 import com.algaworks.algafood.client.model.RestauranteResumoModel;
+import com.algaworks.algafood.client.model.input.RestauranteInput;
 
 import lombok.AllArgsConstructor;
 
@@ -15,17 +19,33 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class RestauranteClient {
 
-	private static final String RESOURCE_PATH = "/restaurantess";
+	private static final String RESOURCE_PATH = "/restaurantes";
 	
 	private RestTemplate restTemplate;
 	private String url;
 	
 	public List<RestauranteResumoModel> listar() {
-		URI resourceUri = URI.create(url + RESOURCE_PATH);
+		try {
+			URI resourceUri = URI.create(url + RESOURCE_PATH);
+			
+			RestauranteResumoModel[] restaurantes = restTemplate
+					.getForObject(resourceUri, RestauranteResumoModel[].class);
+			
+			return Arrays.asList(restaurantes);			
+		} catch (RestClientResponseException e) {
+			throw new ClientApiException(e.getMessage(), e);
+		}
+	}
+	
+	//16.12. Desafio: Implementando uma requisição POST no client Java
+	public RestauranteModel adicionar(RestauranteInput restaurante) {
+		var resourceUri = URI.create(url + RESOURCE_PATH);
 		
-		RestauranteResumoModel[] restaurantes = restTemplate
-				.getForObject(resourceUri, RestauranteResumoModel[].class);
-		
-		return Arrays.asList(restaurantes);
+		try {
+			return restTemplate
+					.postForObject(resourceUri, restaurante, RestauranteModel.class);
+		} catch (HttpClientErrorException e) {
+			throw new ClientApiException(e.getMessage(), e);
+		}
 	}
 }
--- src/main/java/com/algaworks/algafood/client/model/Problem.java
+++ src/main/java/com/algaworks/algafood/client/model/Problem.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood.client.model;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import lombok.Data;
 
@@ -10,5 +12,15 @@ public class Problem {
 	private Integer status;
 	private OffsetDateTime timestamp;
 	private String userMessage;
+
+	private List<Object> objects = new ArrayList<>();
 	
+	//16.12. Desafio: Implementando uma requisição POST no client Java
+	@Data
+	public static class Object {
+		
+		private String name;
+		private String userMessage;
+		
+	}
 }
